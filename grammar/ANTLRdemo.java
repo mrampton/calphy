1732f1f2196373a00cd91cd6b6e2b26c5196b7d1
mrampton <mrampton@gmail.com>
2015-04-18 17:35:17 -0400

    Have MyListener class extend CalphyBaseListener; hello world essentially working

--- grammar/ANTLRdemo.java
+++ grammar/ANTLRdemo.java
@@ -3,9 +3,8 @@ import org.antlr.v4.runtime.tree.*;
 
 public class ANTLRDemo {
     public static void main(String[] args) throws Exception {
-		String s = 	"void main(){ " +
-					" print('hello world')" 		+
-					"}";
+		// String s = 	"void main(){ print('hello world'); }";
+		String s = 	"void main(){ print(2 + 3); }";
 
         ANTLRInputStream in = new ANTLRInputStream(s);
         CalphyLexer lexer = new CalphyLexer(in);
@@ -14,7 +13,7 @@ public class ANTLRDemo {
 		
 		CalphyParser.FunctionDefinitionContext calphyContext = parser.functionDefinition();
 		ParseTreeWalker walker = new ParseTreeWalker();
-		CalphyBaseListener listener = new CalphyBaseListener();
+		MyListener listener = new MyListener();
 		walker.walk(listener, calphyContext);
        	//parser.functionDefinition();
     }
