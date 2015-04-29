49013c7d80f873da471289ffa82069a4c478967e
mrampton <mrampton@gmail.com>
2015-04-29 11:53:34 -0400

    Rename a few files to mirror our Language Tutorial

--- grammar/Compiler.java
+++ grammar/Compiler.java
@@ -1,7 +1,7 @@
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
 
-public class ANTLRDemo {
+public class Compiler {
     public static void main(String[] args) throws Exception {
 		CalphyLexer lexer = new CalphyLexer( new ANTLRFileStream(args[0]));
         CommonTokenStream tokens = new CommonTokenStream(lexer);
