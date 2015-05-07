75d43360c27ff752419edfe7e070cbe498571de6
System Administrator <root@dyn-160-39-141-84.dyn.columbia.edu>
2015-05-07 17:13:50 -0400

    Acceleration + Velocity- compiled

--- grammar/Acceleration.java
+++ grammar/Acceleration.java
@@ -1,66 +1,67 @@
 import java.util.*;
 import java.io.*;
+import java.lang.*;
 class Acceleration {
-	// double value_x;
-	// double value_y;
-	// String units;
-	//
-	// Acceleration(double value_x, double value_y) {
-	// 	this.value_x = value_x;
-	// 	this.value_y = value_y;
-	// 	this.units = "m/s^2";
-	// }
-	//
-	// Acceleration(double value_x, double value_y, String units) {
-	// 	String unitlist = units.split("/");
-	//
-	// 	switch(unitlist[0]) {
-	// 		case "nm":
-	// 			this.value_x = value_x*Math.exp(10,-9);
-	// 			this.value_y = value_y*Math.exp(10,-9);
-	// 			break;
-	// 		case "mm":
-	// 			this.value_x = value_x*Math.exp(10,-3);
-	// 			this.value_y = value_y*Math.exp(10,-3);
-	// 			break;
-	// 		case "cm":
-	// 			this.value_x = value_x*Math.exp(10,-2);
-	// 			this.value_y = value_y*Math.exp(10,-2);
-	// 			break;
-	// 		case "m":
-	// 			this.value_x = value_x;
-	// 			this.value_y = value_y;
-	// 			break;
-	// 		case "km":
-	// 			this.value_x = value_x*1000;
-	// 			this.value_y = value_y*1000;
-	// 			break;
-	// 		default:
-	// 			System.out.println("error\n");
-	// 	}
-	//
-	// 	unitlist[1] = unitlist[1].substring(0, unitlist[1].length - 2);		//trim trailing '^2'
-	// 	switch(unitlist[1]) {
-	// 		case "ns":
-	// 			this.value_x = value_x/(Math.exp(10,-9)*Math.exp(10,-9));
-	// 			this.value_y = value_y/(Math.exp(10,-9)*Math.exp(10,-9));
-	// 			break;
-	// 		case "ms":
-	// 			this.value_x = value_x/(Math.exp(10,-3)*Math.exp(10,-3));
-	// 			this.value_y = value_y/(Math.exp(10,-3)*Math.exp(10,-3));
-	// 			break;
-	// 		case "s":
-	// 			this.value_x = value_x;
-	// 			this.value_y = value_y;
-	// 			break;
-	// 		case "h":
-	// 			this.value_x = value_x/(60*60);
-	// 			this.value_y = value_y/(60*60);
-	// 			break;
-	// 		default:
-	// 			System.out.println("error\n");
-	// 	}
-	//
-	// 	this.units = "m/s^2";
-	// }
+	  double value_x;
+	  double value_y;
+	  String units;
+	 
+	  Acceleration(double value_x, double value_y) {
+	  	this.value_x = value_x;
+	  	this.value_y = value_y;
+	  	this.units = "m/s^2";
+	  }
+	 
+	  Acceleration(double value_x, double value_y, String units) {
+	  	String [] unitlist = units.split("/");
+	 
+	  	switch(unitlist[0]) {
+	  		case "nm":
+	  			this.value_x = value_x*Math.pow(10,-9);
+	  			this.value_y = value_y*Math.pow(10,-9);
+	  			break;
+	  		case "mm":
+	  			this.value_x = value_x*Math.pow(10,-3);
+	  			this.value_y = value_y*Math.pow(10,-3);
+	  			break;
+	  		case "cm":
+	  			this.value_x = value_x*Math.pow(10,-2);
+	  			this.value_y = value_y*Math.pow(10,-2);
+	  			break;
+	  		case "m":
+	  			this.value_x = value_x;
+	  			this.value_y = value_y;
+	  			break;
+	  		case "km":
+	  			this.value_x = value_x*1000;
+	  			this.value_y = value_y*1000;
+	  			break;
+	  		default:
+	  			System.out.println("error\n");
+	  	}
+	 
+	  	unitlist[1] = unitlist[1].substring(0, unitlist[1].length() - 2);		 //trim trailing '^2'
+	  	switch(unitlist[1]) {
+	  		case "ns":
+	  			this.value_x = value_x/(Math.pow(10,-9)*Math.pow(10,-9));
+	  			this.value_y = value_y/(Math.pow(10,-9)*Math.pow(10,-9));
+	  			break;
+	  		case "ms":
+	  			this.value_x = value_x/(Math.pow(10,-3)*Math.pow(10,-3));
+	  			this.value_y = value_y/(Math.pow(10,-3)*Math.pow(10,-3));
+	  			break;
+	  		case "s":
+	  			this.value_x = value_x;
+	  			this.value_y = value_y;
+	  			break;
+	  		case "h":
+	  			this.value_x = value_x/(60*60);
+	  			this.value_y = value_y/(60*60);
+	  			break;
+	  		default:
+	  			System.out.println("error\n");
+	  	}
+	 
+	  	this.units = "m/s^2";
+	  }
 }
