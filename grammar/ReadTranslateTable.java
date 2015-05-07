461620815e31ea354b1255efd56dd910f428a2e5
System Administrator <root@dyn-160-39-141-84.dyn.columbia.edu>
2015-05-07 17:42:44 -0400

    Fixed ReadTranslateTable

--- grammar/ReadTranslateTable.java
+++ grammar/ReadTranslateTable.java
@@ -16,25 +16,16 @@ class ReadTranslateTable {
 		String line;
 
 		try {
-	    	Scanner scan = new Scanner(new File(filename));
+	    		Scanner scan = new Scanner(new File(filename));
 
-	    	while (scan.hasNextLine()){
-	    		line = scan.nextLine();
-	    		lines = line.split("\t");
+	    		while (scan.hasNextLine()) {
+	    			line = scan.nextLine();
+	    			lines = line.split("\t");
 				list.put(lines[0], lines[1]);
 			}
 
-			Iterator it = list.entrySet().iterator();
-			while (it.hasNext()) {
-	    		Map.Entry pair = (Map.Entry)it.next();
-	    		System.out.println(pair.getKey() + " = " + pair.getValue());
-				// #Question from: MR; why do we have it.remove()? it seems to destroy the contents of list
-	    		// it.remove(); // avoids a ConcurrentModificationException
-			}
-			
 		} catch (FileNotFoundException ex) {
-			
+			System.out.println(ex);
 		}
-
 	}
 }
