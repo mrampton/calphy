20c321d96853ee4bbed68f28ee388cd48de731fc
Qi Yang <qy2152@columbia.edu>
2015-05-08 01:16:48 -0400

    Uploaded MyListener2. MyListener2 stores objects(NodeProperty) in ParseTreeProperty for each Node. The object is constructed and store during enterEveryRule. Symbol table pointer is store in NodeProperty object and modified during exitEveryRule()

--- grammar/Compiler.java
+++ grammar/Compiler.java
@@ -3,23 +3,23 @@ import org.antlr.v4.runtime.tree.*;
 
 public class Compiler {
     public static void main(String[] args) throws Exception {
-		if (args.length == 1) {
-			CalphyLexer lexer = new CalphyLexer( new ANTLRFileStream(args[0]));
-	        CommonTokenStream tokens = new CommonTokenStream(lexer);
-	        CalphyParser parser = new CalphyParser(tokens);
+	if (args.length == 1) {
+		CalphyLexer lexer = new CalphyLexer( new ANTLRFileStream(args[0]));
+	    CommonTokenStream tokens = new CommonTokenStream(lexer);
+	    CalphyParser parser = new CalphyParser(tokens);
 		
-			CalphyParser.ProgramContext calphyContext = parser.program();
-			ParseTreeWalker walker = new ParseTreeWalker();
-			MyListener listener = new MyListener();
-			walker.walk(listener, calphyContext);
+		CalphyParser.ProgramContext calphyContext = parser.program();
+		ParseTreeWalker walker = new ParseTreeWalker();
+		MyListener listener = new MyListener();
+		//MyListener2 listener = new MyListener2();
+		walker.walk(listener, calphyContext);
 			
-			// TODO: this print should be writing to a file
-			System.out.println(listener.values.get(calphyContext));
+		// TODO: this print should be writing to a file
+		//System.out.println(listener.values.get(calphyContext));
 			
-		} else {
-			System.out.println("usage: java Compiler source-code.calphy");
-			System.exit(1);
-		}
-		
+	} else {
+		System.out.println("usage: java Compiler source-code.calphy");
+		System.exit(1);
+	}	
     }
 }
