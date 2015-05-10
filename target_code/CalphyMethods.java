b5d8c9678f76d1cf136285dfbc171e02f93adff1
mrampton <mrampton@gmail.com>
2015-05-10 16:22:49 -0400

    Subtract for vector types

--- target_code/CalphyMethods.java
+++ target_code/CalphyMethods.java
@@ -5,6 +5,45 @@ public class CalphyMethods extends PhysicsMethods {
 	public static PhysicsScalarType _SUB(PhysicsScalarType lhs, PhysicsScalarType rhs) {
 		return _ADD(lhs, rhs.value * -1);
 	}
+
+	public static PhysicsScalarType _SUB(PhysicsScalarType lhs, double rhs) {
+		return _ADD(lhs, rhs * -1);
+	}
+	
+	public static PhysicsScalarType _SUB(double lhs, PhysicsScalarType rhs) {
+		PhysicsScalarType flip =  _SUB(rhs, lhs);
+		flip.value = flip.value * -1;
+		return flip;
+	}
+	
+	public static PhysicsScalarType _SUB(double lhs, String units, PhysicsScalarType rhs) {
+		if (rhs instanceof Mass) {
+			return _SUB(new Mass(lhs, units), rhs);
+		} else if (rhs instanceof Power) {
+			return _SUB(new Power(lhs, units), rhs);
+		} else if (rhs instanceof Time) {
+			return _SUB(new Time(lhs, units), rhs);
+		}
+		return null;
+	}
+	
+	public static PhysicsScalarType _SUB(PhysicsScalarType lhs, double rhs, String units) {
+		if (lhs instanceof Mass) {
+			return _SUB(lhs, new Mass(rhs, units));
+		} else if (lhs instanceof Power) {
+			return _SUB(lhs, new Power(rhs, units));
+		} else if (lhs instanceof Time) {
+			return _SUB(lhs, new Time(rhs, units));
+		}
+		return null;
+	}
+	
+	//
+	// public static PhysicsScalarType _SUB(PhysicsScalarType lhs, double rhs, String units) {
+	// 	System.out.println(lhs.value + "-" + rhs);
+	// 	return _SUB(rhs, units, lhs);
+	// }
+	
 	
 	public static PhysicsScalarType _ADD(PhysicsScalarType lhs, PhysicsScalarType rhs) {
 		if (lhs instanceof Mass) {
