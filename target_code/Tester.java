9e72a8eb2c249d937c9771e4251d4f2218e71e69
mrampton <mrampton@gmail.com>
2015-05-10 13:31:46 -0400

    CalphyMethods for add

--- target_code/Tester.java
+++ target_code/Tester.java
@@ -4,7 +4,7 @@ public class Tester extends CalphyMethods {
 		Acceleration a = new Acceleration(10,0);
 		Mass m = getMass(f,a);
 		Mass m1 = new Mass(10);
-		Mass m2 = _ADD(m,m1);
+		Mass m2 =(Mass)_ADD(m,m1);
 		System.out.println(m.value);
 		System.out.println(m1.value);
 		System.out.println(m2.value);
