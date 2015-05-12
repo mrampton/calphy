0ba28dda208eaa06452d3610178f271503e7e9fd
mrampton <mrampton@gmail.com>
2015-05-12 12:48:09 -0400

    Allow compiler to name class and file of generated code

--- grammar/Compiler.java
+++ grammar/Compiler.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 public class Compiler {
 	public static void main(String[] args) throws Exception {
 		if (args.length >= 1) {
+			String defaultClass = "CalphyClass";
 			CalphyLexer lexer = new CalphyLexer( new ANTLRFileStream(args[0]));
 	    CommonTokenStream tokens = new CommonTokenStream(lexer);
 	    CalphyParser parser = new CalphyParser(tokens);
@@ -14,10 +15,13 @@ public class Compiler {
 			MyListener listener = new MyListener();
 			walker.walk(listener, calphyContext);
 			
-			String outputClass = args.length == 1 ? "CalphyClass.java" : args[1] + ".java";
+			String outputClass = args.length == 1 ? defaultClass + ".java" : args[1] + ".java";
 		
 			PrintWriter writer = new PrintWriter(outputClass, "UTF-8");
-			writer.print(listener.treeProperty.get(calphyContext).value);
+			if (args.length == 2)
+				writer.print(listener.treeProperty.get(calphyContext).value.replaceAll(defaultClass, args[1]));
+			else
+				writer.print(listener.treeProperty.get(calphyContext).value);
 			writer.close();
 			
 		} else {
